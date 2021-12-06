285d4bc3ee837385195d5390dcafa87ea3948d20
Kateryna Shkinova <shkiniova@gmail.com>
2021-12-06 15:29:04 -0500

    Initial commit

---
+++ music_pred.py
@@ -0,0 +1,159 @@
+import numpy as np
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+import lightgbm as lgb
+import xgboost as xgb
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA, FastICA
+from sklearn.feature_selection import f_classif, SelectKBest
+
+def main(dataset_path, save_path, dev):
+    train_data = pd.read_csv(dataset_path + 'train_data.csv')
+    train_target = pd.read_csv(dataset_path + 'train_target.csv')
+
+    valid_data = pd.read_csv(dataset_path + 'valid_data.csv')
+    valid_target = pd.read_csv(dataset_path + 'valid_target.csv')
+
+    x_train = train_data.to_numpy()
+    y_train = train_target.to_numpy()
+    n1, dim1 = y_train.shape
+    y_train = y_train.reshape((n1,))
+
+    x_valid = valid_data.to_numpy()
+    y_valid = valid_target.to_numpy()
+    n2, dim2 = y_valid.shape
+    y_valid = y_valid.reshape((n2,))
+    
+    print('Starting training')
+
+    # Logistic Regression
+    log_reg = LogisticRegression(solver='lbfgs', multi_class='auto')
+    print('\n---------- Running Logistic... ----------')
+    log_reg.fit(x_train, y_train)
+
+    pickle.dump(log_reg, open(save_path + 'log_reg.sav','wb')) # save model
+
+    importance = log_reg.coef_[0]
+    get_feature_importance(importance, train_data.columns, 'logreg_importance.png',)
+    evaluate_model(log_reg,x_train,y_train,x_valid,y_valid)
+    
+    # SVM
+    model = svm.LinearSVC(max_iter=1000)
+    print('\n---------- Running SVM... ----------')
+    model.fit(x_train,y_train)
+
+    pickle.dump(model, open(save_path + 'svm.sav','wb')) # save model
+
+    importance = model.coef_[0]
+    get_feature_importance(importance, train_data.columns, 'svm_importance.png')
+    print('SVM score on train: ', model.score(x_train,y_train))
+    print('SVM score on valid: ', model.score(x_valid,y_valid))
+
+    # Decision Tree
+    tree = DecisionTreeClassifier(ccp_alpha=0.0, class_weight=None, criterion='gini',
+                       max_depth=10, max_features=None, max_leaf_nodes=84,
+                       min_impurity_decrease=0.0, min_impurity_split=None,
+                       min_samples_leaf=1, min_samples_split=5,
+                       min_weight_fraction_leaf=0.0, presort='deprecated',
+                       random_state=None, splitter='best')
+    print('\n---------- Running Decision Tree... ----------')
+    tree.fit(x_train, y_train)
+
+    pickle.dump(tree, open(save_path + 'tree.sav','wb')) # save model
+
+    importance = tree.feature_importances_
+    get_feature_importance(importance, train_data.columns, 'tree_importance.png')
+    evaluate_model(tree,x_train,y_train,x_valid,y_valid)
+    print('DecisionTree score on train: ', tree.score(x_train,y_train))
+    print('DecisionTree score on valid: ', tree.score(x_valid,y_valid))
+
+    # Random Forest
+    forest = RandomForestClassifier(bootstrap=True, ccp_alpha=0.0, class_weight='balanced',
+                       criterion='gini', max_depth=25, max_features='auto',
+                       max_leaf_nodes=None, max_samples=None,
+                       min_impurity_decrease=0.0, min_impurity_split=None,
+                       min_samples_leaf=1, min_samples_split=10,
+                       min_weight_fraction_leaf=0.0, n_estimators=1000,
+                       n_jobs=-1, oob_score=False, random_state=23, verbose=0,
+                       warm_start=False)
+    print('\n---------- Running Random Forest... ----------')
+    forest.fit(x_train, y_train)
+
+    pickle.dump(forest, open(save_path + 'forest.sav','wb')) # save model
+
+    importance = forest.feature_importances_
+    get_feature_importance(importance, train_data.columns, 'forest_importance.png')
+    evaluate_model(forest,x_train,y_train,x_valid,y_valid)
+    print('RandomForest score on train: ', forest.score(x_train,y_train))
+    print('RandomForest score on valid: ', forest.score(x_valid,y_valid))
+
+    # XgBoost
+
+    model = xgb.XGBModel(objective ='binary:logistic', learning_rate = 0.1, colsample_bytree = 0.5, max_depth = 10, alpha = 10, n_estimators = 50)
+    print('\n---------- Running XgBoost... ----------')
+    model.fit(x_train, y_train, eval_set = [(x_valid,y_valid)], eval_metric = 'auc')
+    pickle.dump(model,open(save_path + 'xgb_model.sav', 'wb'))
+
+    # model = pickle.load(open('xgb_model.sav', 'rb'))
+    print(accuracy_score(y_valid, model.predict(x_valid)>0.5))
+    print(accuracy_score(y_train, model.predict(x_train)>0.5))
+    importance = model.feature_importances_
+    get_feature_importance(importance, train_data.columns, 'xgb_importance.png')
+
+    # LightGBM
+
+    model = lgb.LGBMModel(learning_rate = 0.3, max_depth = 15, num_leaves = 250, objective = 'binary', n_estimators=200) #0.61
+    print('\n---------- Running LightGBM... ----------')
+    model.fit(x_train,y_train, eval_set = [(x_valid,y_valid)], eval_metric = 'auc')
+    pickle.dump(model,open(save_path + 'lgbm_model.sav', 'wb'))
+
+    # model = pickle.load(open('./predictions/lgbm_model.sav', 'rb'))
+    print(np.mean((model.predict(x_valid, num_iteration=model.best_iteration_)>0.5)==y_valid))
+    print(np.mean((model.predict(x_train, num_iteration=model.best_iteration_)>0.5)==y_train))
+    importance = model.feature_importances_
+    get_feature_importance(importance, train_data.columns, 'lgbm_importance.png')
+
+    # PCA ICA KBest
+    pca = PCA(n_components=10)
+    ica = FastICA(n_components=10)
+    kbest = SelectKBest(f_classif, k=15)
+
+    feature_selection(pca, x_train, y_train, x_valid, y_valid)
+    feature_selection(ica, x_train, y_train, x_valid, y_valid)
+    feature_selection(kbest, x_train, y_train, x_valid, y_valid)
+
+    
+def get_feature_importance(importance,labels,save_file,save_dir='./plots/'):
+    plt.figure(figsize=(15,10))
+    plt.xlabel('features',fontsize=30)
+    plt.ylabel('importance',fontsize=30)
+    plt.xticks(rotation='90') # rotate labels
+    plt.bar(labels, importance)
+    plt.savefig(save_dir + save_file)
+
+def feature_selection(model, x_train, y_train, x_valid, y_valid):
+    selection = model.fit(x_train, y_train)
+    x_train_new = selection.transform(x_train)
+    x_valid_new = selection.transform(x_valid)
+
+    model = lgb.LGBMModel(learning_rate = 0.3, max_depth = 15, num_leaves = 250, objective = 'binary', n_estimators=200)
+
+    model.fit(x_train_new,y_train, eval_set = [(x_valid_new,y_valid)], eval_metric = 'auc')
+    print(np.mean((model.predict(x_valid_new, num_iteration=model.best_iteration_)>0.5)==y_valid))
+    print(np.mean((model.predict(x_train_new, num_iteration=model.best_iteration_)>0.5)==y_train))
+
+def evaluate_model(model,x_train,y_train,x_valid,y_valid):
+    print('Accuracy on train: ', model.score(x_train,y_train))
+    print('Accuracy on valid: ', model.score(x_valid,y_valid))
+    print('roc auc on train:', roc_auc_score(y_train, model.predict_proba(x_train)[:,1]))
+    print('roc auc on valid:', roc_auc_score(y_valid, model.predict_proba(x_valid)[:,1]))
+
+if __name__ == '__main__':
+    main(dataset_path='./training_data/', save_path='./models/', dev=True)
---
+++ process_datasets.py
@@ -0,0 +1,181 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn import preprocessing
+
+def main(dataset_path, dev):
+     
+    save_path = './training_data/'
+
+    train_set = pd.read_csv(dataset_path + 'train.csv', na_values=[''])
+
+    train_set, valid_set = split_dataset(train_set)
+
+    train_data, train_target = merge_datasets(train_set, dataset_path)
+    valid_data, valid_target = merge_datasets(valid_set, dataset_path)
+
+    if dev:
+        get_plots(train_data,valid_data)
+
+    train_data = fill_NA_values(train_data)
+    valid_data = fill_NA_values(valid_data)
+
+    train_data = extract_features(train_data)
+    valid_data = extract_features(valid_data)
+
+    train_data, valid_data = encode_features(train_data, valid_data, './encoders/')
+
+    train_data.to_csv(save_path + 'train_data.csv', index=False)
+    train_target.to_csv(save_path + 'train_target.csv', index=False)
+    valid_data.to_csv(save_path + 'valid_data.csv', index=False)
+    valid_target.to_csv(save_path + 'valid_target.csv', index=False)
+
+
+def split_dataset(train):
+
+    print('\ntarget = 1 (in %):', (train['target']==1).sum()/train.shape[0]*100)
+    print('target = 0 (in %):', (train['target']==0).sum()/train.shape[0]*100)
+
+    train_i = int(train.shape[0]*0.9)
+    print('\nsplit index: ', train_i)
+
+    train_set = train.iloc[:train_i, :]
+    valid_set = train.iloc[train_i:, :]
+
+    print('\nTraining set shape: ', train_set.shape)
+    print('Validation set shape: ', valid_set.shape)
+
+    return train_set, valid_set
+
+
+def merge_datasets(set, path):
+    '''
+        Merges train/valid dataset with members, songs, and song_extra_info
+    '''
+    members = pd.read_csv(path + 'members.csv')
+    songs = pd.read_csv(path + 'songs.csv')
+    songs_extra = pd.read_csv(path + 'song_extra_info.csv')
+
+    merged = set.merge(members, how='left', on='msno')\
+        .merge(songs, how='left', on='song_id')\
+        .merge(songs_extra, how='left', on='song_id')
+    
+    features = merged.loc[:, merged.columns != 'target']
+    target = merged.loc[:, 'target']
+
+    return features, target
+
+def fill_NA_values(train_data):
+    train_data['gender'].fillna('no_gender', inplace=True)
+    train_data['genre_ids'].fillna(0, inplace=True)
+    train_data['artist_name'].fillna('no_artist', inplace=True)
+    train_data['composer'].fillna('no_composer', inplace=True)
+    train_data['lyricist'].fillna('no_lyricist', inplace=True)
+    train_data['language'].fillna(0, inplace=True)
+    train_data['name'].fillna('no_name', inplace=True)
+    train_data['song_length'].fillna(0, inplace=True)
+    train_data['isrc'].fillna('no_isrc', inplace=True)
+    # 'source_system_tab', 'source_screen_name', 'source_type',
+    train_data['source_system_tab'].fillna('no_system_tab', inplace=True)
+    train_data['source_screen_name'].fillna('no_screen_name', inplace=True)
+    train_data['source_type'].fillna('no_type', inplace=True)
+    return train_data
+
+
+def extract_features(train_data):
+
+    # filter age to remove outliers
+    train_data['age'] = train_data['bd'].apply(lambda x: np.int8(x) if (x > 0 and x <= 80) else 0)
+
+    # extract year, month, day from date features
+    train_data['registration_init'] = pd.to_datetime(train_data['registration_init_time'],format='%Y%m%d')
+    train_data['registration_year'] = pd.DatetimeIndex(train_data['registration_init']).year
+    train_data['registration_month'] = pd.DatetimeIndex(train_data['registration_init']).month
+    train_data['registration_year'] = pd.DatetimeIndex(train_data['registration_init']).day
+
+    train_data['expiration'] = pd.to_datetime(train_data['expiration_date'],format='%Y%m%d')
+    train_data['expiration_year'] = pd.DatetimeIndex(train_data['expiration']).year
+    train_data['expiration_month'] = pd.DatetimeIndex(train_data['expiration']).month
+    train_data['expiration_year'] = pd.DatetimeIndex(train_data['expiration']).day
+
+    # calculate total duration of membership
+    train_data['membership_period'] = train_data['expiration'] - train_data['registration_init']
+
+    # get main genre and count the number of genres
+    train_data['main_genre'] = train_data['genre_ids'].apply(lambda x: str(x).split('|')[0])
+    train_data['genre_number'] = train_data['genre_ids'].apply(lambda x: len(str(x).split('|')))
+
+    # do the same for artist, composer, lyricist
+    train_data['main_artist'] = train_data['artist_name'].apply(lambda x: str(x).split('|')[0])
+    train_data['artist_number'] = train_data['artist_name'].apply(lambda x: len(str(x).split('|')))
+
+    train_data['main_composer'] = train_data['composer'].apply(lambda x: str(x).split('|')[0])
+    train_data['composer_number'] = train_data['composer'].apply(lambda x: len(str(x).split('|')))
+
+    train_data['main_lyricist'] = train_data['lyricist'].apply(lambda x: str(x).split('|')[0])
+    train_data['lyricist_number'] = train_data['lyricist'].apply(lambda x: len(str(x).split('|')))
+
+    # get year of release and country from isrc
+    train_data['release_year'] = train_data['isrc'].apply(lambda x: np.int8(str(x)[5:7]) if (x != 'no_isrc') else 0)
+    train_data['country'] = train_data['isrc'].apply(lambda x: str(x)[:2] if (x != 'no_isrc') else 'no_country')
+
+    # remove unnecessary columns
+    train_data = train_data.drop(columns=['registration_init_time', 'registration_init', 'expiration_date', 'expiration', \
+        'genre_ids', 'artist_name', 'composer', 'lyricist', 'isrc', 'bd'], axis=1)
+    
+    return train_data
+
+def encode_features(train_data, val_data, save_path):
+    total_data = pd.concat([train_data, val_data])
+
+    # perform standardization
+    for col in train_data.select_dtypes([np.int64,np.float64]):
+        # print('standardizing column ', col)
+        s = preprocessing.StandardScaler()
+        s.fit(total_data[col].to_numpy().reshape(-1,1))
+        train_data[col] = s.transform(train_data[col].to_numpy().reshape(-1,1))
+        val_data[col] = s.transform(val_data[col].to_numpy().reshape(-1,1))
+
+    # encode all non-numeric features
+    for col in train_data.select_dtypes([np.object0, np.timedelta64]):
+        # print('encoding column', col)
+        l = preprocessing.LabelEncoder()
+        l.fit(total_data[col].to_numpy())
+        # np.save(save_path+col+'_classes.npy', l.classes_)
+
+        train_data[col] = l.transform(train_data[col].to_numpy())
+        val_data[col] = l.transform(val_data[col].to_numpy())
+    
+    return train_data, val_data
+
+def get_plots(train_data, valid_data):
+    # generating tables and plots for the report
+    data = pd.concat([train_data,valid_data])
+    print('\nTrain dataset columns:\n')
+    print(train_data.info())
+    print('\nNull columns:\n')
+    print(data.isnull().sum() * 100 / len(train_data))
+
+    print('\nTotal number of users:', data['msno'].nunique())
+    print('Tontal number of songs:', data['song_id'].nunique())
+
+    # user cold-start
+    song_count = data.groupby('msno').count()['song_id'] # song count per user
+    print('\nSong count per user: mean {:.2f}, max {}, min {}'.format(song_count.mean(), song_count.max(), song_count.min()))
+    song_count.hist(bins=100, range=[0,1000])
+    plt.xlabel('song count per user')
+    plt.ylabel('number of users')
+    plt.savefig('./plots/user_coldstart.png')
+    print('Number of users with < 10 song listening history: ', song_count.where(song_count<10).count())
+    # song cold-start
+    user_count = data.groupby('song_id').count()['msno'] # user count per song
+    print('\nUser count per song: mean {:.2f}, max {}, min {}'.format(user_count.mean(), user_count.max(), user_count.min()))
+    user_count.hist(bins=100, range=[0,150])
+    plt.xlabel('user count per song')
+    plt.ylabel('number of songs')
+    plt.savefig('./plots/song_coldstart.png')
+    print('Number of songs with < 5 users listened to it: ', user_count.where(user_count<5).count())   
+
+if __name__ == '__main__':
+    main(dataset_path='./datasets/', dev=True)
